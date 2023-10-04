5c267591f0b0a14b4ec1342c82f038b27838ba7f
Tenshy <thiagogonc4@gmail.com>
2023-10-03 21:04:02 -0300

    init project

---
+++ src/main/java/com/tenshy/wishlist/Tenshy/Wishlist/TenshyWishlistApplication.java
@@ -0,0 +1,13 @@
+package com.tenshy.wishlist.Tenshy.Wishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenshyWishlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenshyWishlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenshy/wishlist/Tenshy/Wishlist/TenshyWishlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenshy.wishlist.Tenshy.Wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenshyWishlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
