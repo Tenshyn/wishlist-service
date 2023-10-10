a5ffbdc9d3bd34d055dbc99b4b2b6075ae4a4c57
Tenshy <thiagogonc4@gmail.com>
2023-10-09 21:34:00 -0300

    feat: created exception to throw if specified user does not exist

---
+++ src/main/java/com/tenshy/wishlist/Tenshy/Wishlist/wishlist/exceptions/UserDoesNotExistException.java
@@ -0,0 +1,9 @@
+package com.tenshy.wishlist.Tenshy.Wishlist.wishlist.exceptions;
+
+public class UserDoesNotExistException extends RuntimeException {
+
+    public UserDoesNotExistException(String msg) {
+        super(msg);
+    }
+
+}
