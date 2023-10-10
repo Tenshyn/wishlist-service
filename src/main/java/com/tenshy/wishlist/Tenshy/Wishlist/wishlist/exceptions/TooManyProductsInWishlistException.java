86e897886046c559cbae0756b2a1cef8804d28d6
Tenshy <thiagogonc4@gmail.com>
2023-10-09 21:38:59 -0300

    feat: created necessary exception to validate product being added to wishlist

---
+++ src/main/java/com/tenshy/wishlist/Tenshy/Wishlist/wishlist/exceptions/TooManyProductsInWishlistException.java
@@ -0,0 +1,9 @@
+package com.tenshy.wishlist.Tenshy.Wishlist.wishlist.exceptions;
+
+public class TooManyProductsInWishlistException extends RuntimeException {
+
+    public TooManyProductsInWishlistException(String msg) {
+        super(msg);
+    }
+
+}
---
+++ src/main/java/com/tenshy/wishlist/Tenshy/Wishlist/wishlist/exceptions/UserAlreadyWishlistProduct.java
@@ -0,0 +1,9 @@
+package com.tenshy.wishlist.Tenshy.Wishlist.wishlist.exceptions;
+
+public class UserAlreadyWishlistProduct extends RuntimeException {
+
+    public UserAlreadyWishlistProduct(String msg) {
+        super(msg);
+    }
+
+}
